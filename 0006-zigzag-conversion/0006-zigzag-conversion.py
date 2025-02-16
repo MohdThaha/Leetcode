a63d09b7154958d6b654472b87df807a7bbf171b
Mohammed Thaha <165124426+MohdThaha@users.noreply.github.com>
2025-02-16 14:50:24 +0530

    Time: 7 ms (89.7%), Space: 17.9 MB (51.28%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,23 +1,20 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
 
-        if numRows == 1 or numRows >= len(s):
+        if numRows == 1 or numRows >=len(s):
             return s
         
-        rows = [""] * numRows
-        index, step = 0, 1  
-        
-        for char in s:
-            rows[index] += char  # Append character to the current row
+        index, step = 0,1
+        ans = ['']*numRows
 
-            # If at the top or bottom, reverse direction
+        for i in s:
+            ans[index] += i
+
             if index == 0:
-                step = 1  # Move downward
-            elif index == numRows - 1:
-                step = -1  # Move upward
+                step = 1
+            elif index == numRows -1:
+                step = -1
             
-            index += step  # Move to the next row
-
-        return "".join(rows)  # Combine all rows into the final result
-
+            index += step
         
+        return "".join(ans)
