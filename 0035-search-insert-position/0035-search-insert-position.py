89a5bf873fcf01e92ba537157a3c082b601af05e
Mohammed Thaha <165124426+MohdThaha@users.noreply.github.com>
2026-01-15 16:54:50 +0530

    Time: 0 ms (100%), Space: 20.1 MB (6.9%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        res = 0
+
+        for i in range (len(nums)):
+
+            if nums[i] < target:
+                res += 1
+
+        return res
