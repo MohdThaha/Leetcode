4f3245a9da0836e8475a1f61796d01b42eac8aa4
Mohammed Thaha <165124426+MohdThaha@users.noreply.github.com>
2026-01-25 02:07:17 +0530

    Time: 15 ms (78.29%), Space: 20 MB (12.61%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,11 @@
+from collections import Counter
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        mp = {}
-        
-        for i in magazine:
-            mp[i] = mp.get(i,0) + 1
-        
-        for j in ransomNote:
-            if j not in mp or mp[j] <= 0:
+        ransom = Counter(ransomNote)
+        mag = Counter(magazine)
+
+        for ch in ransom:
+            if ransom[ch] > mag[ch]:
                 return False
-            mp[j] -= 1
         return True
