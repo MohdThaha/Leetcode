a2e95d2482859e4a06c0223bb145ebd5ed271800
Mohammed Thaha <165124426+MohdThaha@users.noreply.github.com>
2026-02-09 03:51:06 +0530

    Time: 0 ms (100%), Space: 19.3 MB (54.29%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -5,11 +5,9 @@ class Solution:
         while l < r:
             mid = (l + r) // 2
 
-            # Minimum is in the right half
             if nums[mid] > nums[r]:
                 l = mid + 1
             else:
-                # Minimum is at mid or in left half
                 r = mid
 
         return nums[l]
