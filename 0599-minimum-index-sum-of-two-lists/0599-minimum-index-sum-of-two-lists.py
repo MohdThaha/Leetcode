16eb9910a1ad2bce5693eeeb842ab7452d83e1b6
Mohammed Thaha <165124426+MohdThaha@users.noreply.github.com>
2026-02-11 03:07:29 +0530

    Time: 1 ms (96.42%), Space: 19.8 MB (44.39%) - LeetHub

--- 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.py
@@ -4,17 +4,18 @@ class Solution:
         mp = {}
 
         for i in range(len(list1)):
-            for j in range(len(list2)):
-                if list1[i] == list2[j]:
-                    mp[list1[i]] = i + j
+            mp[list1[i]] = i
 
-        minValue = min(mp.values())
-    
+        minval = float('inf')
         res = []
 
-        for i,j in mp.items():
-            if j== minValue:
-                res.append(i)
+        for j in range(len(list2)):
+            if list2[j] in mp:
+                idxsum = j + mp[list2[j]]
 
+                if idxsum < minval:
+                    minval = idxsum
+                    res = [list2[j]]       
+                elif idxsum == minval:
+                    res.append(list2[j])  
         return res
-        
