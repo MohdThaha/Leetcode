fc67ba06238b61e5eaf8df6b83c57f80b2a23584
Mohammed Thaha <165124426+MohdThaha@users.noreply.github.com>
2026-02-13 03:59:37 +0530

    Time: 0 ms (100%), Space: 20.1 MB (41.57%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -9,16 +9,16 @@ class Solution:
         
         result= []
 
-        def dfs(node, level):
+        def bfs(node, level):
             if not node:
                 return
             if len(result) == level:
                 result.append([])
-                
+
             result[level].append(node.val)
 
-            dfs(node.left, level + 1)
-            dfs(node.right, level + 1)
-        dfs(root,0)
+            bfs(node.left, level + 1)
+            bfs(node.right, level + 1)
+        bfs(root,0)
         return result
         
