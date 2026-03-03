6a9789b3e2d2eaf6a2397926b1bbca31cd0e2707
Mohammed Thaha <165124426+MohdThaha@users.noreply.github.com>
2026-03-04 02:23:01 +0530

    Time: 175 ms (79.7%), Space: 35.5 MB (32.6%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        dq = deque()   # stores indices
+        res = []
+
+        for i in range(len(nums)):
+
+            # 1️⃣ Remove elements outside window
+            if dq and dq[0] <= i - k:
+                dq.popleft()
+
+            # 2️⃣ Maintain decreasing order in deque
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+
+            # 3️⃣ Add current index
+            dq.append(i)
+
+            # 4️⃣ Window formed → record max
+            if i >= k - 1:
+                res.append(nums[dq[0]])
+
+        return res
