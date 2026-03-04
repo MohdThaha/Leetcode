c27aba9e3d7782bbe775eb98fb7d6224227e6c52
Mohammed Thaha <165124426+MohdThaha@users.noreply.github.com>
2026-03-05 01:19:31 +0530

    Time: 175 ms (80.14%), Space: 35.4 MB (43.61%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        dq = deque()   # stores indices
+        dq = deque()
         res = []
 
         for i in range(len(nums)):
